2139c1307e6f76ab9e10380c751dbd1fc8a4c23d
costajob <costajob@gmail.com>
2019-03-18 14:47:57 +0100

    Mike - change version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='image_augmenter',
-    version='1.0.0',
+    version='0.1.0',
     author='Michele Costa',
     author_email='costajob@gmail.com',
     description='A python library to augment the images dataset aimed for a ML classification system',
