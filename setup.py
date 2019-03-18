f942b58c3b295c2d2b9772941310805352ddb503
costajob <costajob@gmail.com>
2019-03-18 13:42:27 +0100

    Mike - configure package creation

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='image_augmenter',
+    version='1.0.0',
+    author='Michele Costa',
+    author_email='costajob@gmail.com',
+    description='A python library to augment the images dataset aimed for a ML classification system',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/SopraIt/image_augmenter',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
