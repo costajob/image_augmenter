9705210302d28c2364a1ff88f61a2434d4462559
costajob <costajob@gmail.com>
2019-03-19 08:01:19 +0100

    Mike - moved repository, adde travis

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='image_augmenter',
-    version='0.1.4',
+    version='0.1.5',
     author='Michele Costa',
     author_email='costajob@gmail.com',
     description='A python library to augment the images dataset aimed for a ML classification system',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/SopraIt/image_augmenter',
+    url='https://github.com/costajob/image_augmenter',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3.7',
