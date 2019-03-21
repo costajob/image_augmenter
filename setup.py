c36346de29fe0d5a0f1737723728836e45ce6931
costajob <costajob@gmail.com>
2019-03-21 08:04:18 +0100

    Mike - added install requirements

--- setup.py
+++ setup.py
@@ -7,14 +7,21 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='image_augmenter',
-    version='0.1.5',
+    version='0.1.6',
     author='Michele Costa',
     author_email='costajob@gmail.com',
-    description='A python library to augment the images dataset aimed for a ML classification system',
+    description='A tiny python library to augment the images dataset aimed for a ML classification system',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/costajob/image_augmenter',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'matplotlib>=3.0',
+        'numpy>=1.16',
+        'Pillow>=5.4',
+        'scikit-image>=0.14',
+        'scipy>=1.2'
+    ],
     classifiers=[
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
